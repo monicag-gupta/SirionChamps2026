c3a40c7309e4f4889216d2f1d286a8ca237d86d9
DeveshLokare <135504499+DeveshLokare@users.noreply.github.com>
2026-07-24 17:03:31 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from py4j.java_gateway import JavaGateway
+
+gateway = JavaGateway()
+
+calculator = gateway.entry_point.getCalculator()
+
+print("Addition:", calculator.add(10.0, 20.0))
+print("Subtraction:", calculator.subtract(20.0, 5.0))
+print("Multiplication:", calculator.multiply(6.0, 7.0))
+print("Division:", calculator.divide(20.0, 4.0))
+
+gateway.close()
